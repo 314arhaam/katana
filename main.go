a2d00039cc7d11a12fd162f886fad36ad191d5ba
Parham <prmbas@gmail.com>
2026-04-29 01:17:51 +0000

    check subcommand added to compare size of two files. For fast E2E

--- main.go
+++ main.go
@@ -27,6 +27,14 @@ func main() {
 			fmt.Println("main.go: `merge`:", err)
 			return
 		}
+	case "check":
+		if len(os.Args) != 4 {
+			fmt.Println("main.go: Invalid syntax for `check`: must be katana check <FILE_1> <FILE_2>")
+			return
+		}
+		stat1, _ := os.Stat(os.Args[2])
+		stat2, _ := os.Stat(os.Args[3])
+		fmt.Println(stat1.Size() == stat2.Size())
 	default:
 		fmt.Println("main.go: unknown subcommand")
 	}
