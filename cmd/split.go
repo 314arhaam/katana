ecc9cb63295697377c145603dcefc6db0befed13
Parham <prmbas@gmail.com>
2026-04-29 17:24:45 +0000

    related to Set limit for chunkSize #2 Issue. chunk size zero fixed. bigger than file fixed. too many parts fixed.

--- cmd/split.go
+++ cmd/split.go
@@ -6,11 +6,29 @@ import (
 	"strconv"
 	"io"
 	"path"
+	"math"
 )
 
 func SplitFile(filepath string, chunkSize int) error {
 	// get filename from path
 	filename := path.Base(filepath)
+	// check chunk size non zero
+	if chunkSize == 0 {
+		return fmt.Errorf("split.go: chunkSize cannot be zero")
+	}
+	// get file stat
+	fileStat, err := os.Stat(filepath)
+	if err != nil {
+		return fmt.Errorf("split.go: stat input file:", err)	
+	}
+	// check if chunk size bigger than file size
+	if fileStat.Size() <= int64(chunkSize) {
+		return fmt.Errorf("split.go: chunkSize greater than original file size")
+	}
+	// check if chunk size leads to more than 50 parts
+	if numParts := math.Floor(float64(fileStat.Size()) / float64(chunkSize)) + 1; numParts > 50 {
+		return fmt.Errorf("split.go: chunkSize leads more than 50 parts (%.0f parts), use a greater chunkSize", numParts)
+	}
 	// open a file
 	file, err := os.Open(filepath)
 	if err != nil {
