daf23647b487840494c7ac2518008d9ffd2fa33b
Parham <prmbas@gmail.com>
2026-05-02 18:29:47 +0000

    log.Fatalf used for error signal

--- main.go
+++ main.go
@@ -5,38 +5,34 @@ import (
 	"fmt"
 	"local/katana/cmd"
 	"local/katana/internal/clitools"
+	"log"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "main.go: no subcommand found")
-		return
+		log.Fatalf("main.go: no subcommand found")
 	}
 	switch os.Args[1] {
 	case "split":
 		cli := clitools.SplitCLIArgs{}
 		cli.Init()
 		if err := cmd.SplitFile(*cli.Filepath, (*cli.ChunkSize)*1024); err != nil {
-			fmt.Fprintln(os.Stderr, "main.go: `split`:", err)
-			return
+			log.Fatalf("main.go: `split`:", err)
 		}
 	case "merge":
 		cli := clitools.MergeCLIArgs{}
 		cli.Init()
 		if err := cmd.MergeParts(*cli.Filepath); err != nil {
-			fmt.Fprintln(os.Stderr, "main.go: `merge`:", err)
-			return
+			log.Fatalf("main.go: `merge`:", err)
 		}
 	case "check":
 		if len(os.Args) != 4 {
-			fmt.Fprintln(os.Stderr, "main.go: Invalid syntax for `check`: must be katana check <FILE_1> <FILE_2>")
-			return
+			log.Fatalf("main.go: Invalid syntax for `check`: must be katana check <FILE_1> <FILE_2>")
 		}
 		stat1, _ := os.Stat(os.Args[2])
 		stat2, _ := os.Stat(os.Args[3])
 		fmt.Println(stat1.Size() == stat2.Size())
 	default:
-		fmt.Fprintln(os.Stderr, "main.go: unknown subcommand")
-		return
+		log.Fatalf("main.go: unknown subcommand: %s", os.Args[1])
 	}
 }
